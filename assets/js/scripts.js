2c23746a42f05f34cabc8ec866124ef758da19df
benjaminGuirin <126121809+benjaminGuirin@users.noreply.github.com>
2023-08-10 20:45:30 -0300

    Revert "44"
    This reverts commit a5c44099cff7deaa6ae10a43c93ac6f64a1c7ac7.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,3 @@
-// leer mas
-let i = 0;
-let button = document.querySelector('#button').addEventListener('click', function() {
-    if (!i) {
-        document.getElementById('leerMas').style.display = 'inline';
-        document.getElementById('card').style.height = '800px';
-        document.getElementById('button').innerHTML = 'Leer menos';
-        i = 1;
-    } else {
-        document.getElementById('leerMas').style.display = 'none';
-        document.getElementById('card').style.height = '600px';
-        document.getElementById('button').innerHTML = 'Leer más';
-        i = 0;
-    }
-
-})
-
-//--------------------------------------
 let navbar = document.querySelector('.ul');
 
 document.querySelector('#menu-btn').onclick = ()=>{
@@ -38,7 +20,7 @@ Window.onscroll = ()=>{
 }
 
 // cambiar palabras-----------------------------------------------
-var palabras = ["Disfrutar del viaje", "vivir experiencias", "probar los mejores vinos"]; // Array de palabras
+var palabras = ["Disfrutar del viaje", "vivir experiencias", "probar un buen vino"]; // Array de palabras
 var indice = 0; // Índice de la palabra actual
 
 function cambiarPalabra() {
@@ -205,7 +187,3 @@ function solonumeros(e){
         return false;
     }
 } */
-
-
-
-
